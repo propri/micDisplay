5ecc9ccbf33fd77c72c71dee6118cf42a699fbd8
Wilhelm Mailaender <wilhelm.mailaender@avenga.com>
2023-07-19 10:26:13 +0200

    Initial Commit
    Simple Widget to display if my microphone is muted

---
+++ applet.js
@@ -0,0 +1,73 @@
+const Applet = imports.ui.applet;
+const Mainloop = imports.mainloop;
+// const Soup = imports.gi.Soup;
+// const Settings = imports.ui.settings;
+const Util = imports.misc.util;
+
+let muted = false;
+
+function MicDisplay(metadata, orientation, panel_heigt, instance_id) {
+  this._init(metadata, orientation, panel_heigt, instance_id);
+}
+
+MicDisplay.prototype = {
+  __proto__: Applet.TextIconApplet.prototype,
+
+  _init: function (metadata, orientation, panel_height, instance_id) {
+    Applet.TextIconApplet.prototype._init.call(
+      this,
+      orientation,
+      panel_height,
+      instance_id,
+    );
+
+    this.updateMuted();
+
+    this.startUp(true);
+  },
+
+  updateMuted: function () {
+    if (muted) {
+      this.set_applet_icon_name("micDisplay-muted");
+      this.set_applet_label("M");
+      this.set_applet_tooltip(_("Microphone is muted"));
+    } else {
+      this.set_applet_icon_name("micDisplay-recording");
+      this.set_applet_label("R");
+      this.set_applet_tooltip(_("Microphone is recording"));
+    }
+  },
+
+  on_applet_clicked: function () {
+    // switch muted state
+    Util.spawnCommandLine("/home/wilhelm/bin/tc30_mute_toggle.sh");
+  },
+
+  startUp: function (setupLoop) {
+    if (setupLoop) {
+      this.updateLoop(true);
+    } else {
+      this.updateUI();
+    }
+  },
+
+  updateUI: function () {
+    Util.spawn_async(["/home/wilhelm/bin/is_mic_muted.sh"], (stdout) => {
+      if (stdout.indexOf("yes") > -1) {
+        muted = true;
+      } else {
+        muted = false;
+      }
+    });
+  },
+
+  updateLoop: function () {
+    this.updateUI();
+    Mainloop.timeout_add_seconds(1, this.updateLoop.bind(this));
+    this.updateMuted();
+  },
+};
+
+function main(metadata, orientation, panel_height, instance_id) {
+  return new MicDisplay(metadata, orientation, panel_height, instance_id);
+}
